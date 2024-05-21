57710ae1e9717b5932c3cfded11c441f24734585
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-21 14:59:59 +0200

    create initial Landing page structure

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+const Landing = () => {
+  return (
+    <div>
+      <div>
+        <h1>Book a time for a theory test online</h1>
+        <div>
+          <button type="button">Log In</button>
+          <button type="button">Register</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
