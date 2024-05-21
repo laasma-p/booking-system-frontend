8e90624b346940e1a3d9e1d04d590e35987fc292
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-21 15:33:19 +0200

    add background image

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,11 @@
 const Landing = () => {
   return (
-    <div>
+    <div
+      className="h-dvh bg-cover bg-center flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: "url('https://source.unsplash.com/1600x900/?driving')",
+      }}
+    >
       <div>
         <h1>Book a time for a theory test online</h1>
         <div>
