918554731b8c81c4947eb061689421a786b5e156
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-22 18:19:53 +0200

    add routing to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import Landing from "./components/Landing";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Landing />;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+    </Routes>
+  );
 }
 
 export default App;
