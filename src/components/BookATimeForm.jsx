824ca3e9aeecb894c9db9646f777ac5c87916c67
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-23 16:58:34 +0200

    add styles and disallow access to past dates

--- src/components/BookATimeForm.jsx
+++ src/components/BookATimeForm.jsx
@@ -8,15 +8,33 @@ const BookATimeForm = () => {
   const renderCalendarDays = () => {
     const days = [];
 
-    const date = new Date(
+    const endDate = new Date(
       currentMonth.getFullYear(),
       currentMonth.getMonth() + 1,
       0
     );
 
-    for (let i = 1; i <= date.getDate(); i++) {
+    const today = new Date();
+    const todaysYear = today.getFullYear();
+    const todaysMonth = today.getMonth();
+    const todaysDate = today.getDate();
+
+    for (let i = 1; i <= endDate.getDate(); i++) {
+      const date = new Date(
+        currentMonth.getFullYear(),
+        currentMonth.getMonth(),
+        i
+      );
+
+      const isPastDay = date < new Date(todaysYear, todaysMonth, todaysDate);
+
       days.push(
-        <div key={i} className="p-2 border h-16 cursor-pointer">
+        <div
+          key={i}
+          className={`p-2 border h-16 ${
+            isPastDay ? "bg-gray-100" : "bg-white text-gray-950"
+          }`}
+        >
           {i}
         </div>
       );
@@ -26,11 +44,15 @@ const BookATimeForm = () => {
   };
 
   return (
-    <div>
-      <h1>Book a time</h1>
-      <div>
+    <div className="p-6 max-w-lg bg-white rounded-md shadow-md space-y-4">
+      <h1 className="text-3xl font-semibold text-gray-950 mb-6">Book a time</h1>
+      <div className="grid grid-cols-7 gap-1">
         {daysOfTheWeek.map((day, index) => {
-          return <div key={index}>{day}</div>;
+          return (
+            <div key={index} className="p-2 border h-16 bg-gray-200">
+              {day}
+            </div>
+          );
         })}
         {renderCalendarDays()}
       </div>
