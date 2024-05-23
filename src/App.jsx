6b59c41451990bc1d8639bffdd3ba825d3119dae
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-23 15:35:57 +0200

    render the rest of the components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import "./App.css";
 import Landing from "./components/Landing";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Navigation from "./components/Navigation";
+import Bookings from "./components/Bookings";
+import BookATimeForm from "./components/BookATimeForm";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="book-a-time" element={<BookATimeForm />} />
+      </Routes>
+    </>
   );
 }
 
