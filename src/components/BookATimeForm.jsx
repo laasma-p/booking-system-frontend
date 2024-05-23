36eb59d8618cbffa044c1370a682c4d52c2d0adb
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-23 16:24:52 +0200

    render current month days

--- src/components/BookATimeForm.jsx
+++ src/components/BookATimeForm.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 const daysOfTheWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 const BookATimeForm = () => {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const renderCalendarDays = () => {
+    const days = [];
+
+    const date = new Date(
+      currentMonth.getFullYear(),
+      currentMonth.getMonth() + 1,
+      0
+    );
+
+    for (let i = 1; i <= date.getDate(); i++) {
+      days.push(
+        <div key={i} className="p-2 border h-16 cursor-pointer">
+          {i}
+        </div>
+      );
+    }
+
+    return days;
+  };
+
   return (
     <div>
       <h1>Book a time</h1>
@@ -8,6 +32,7 @@ const BookATimeForm = () => {
         {daysOfTheWeek.map((day, index) => {
           return <div key={index}>{day}</div>;
         })}
+        {renderCalendarDays()}
       </div>
     </div>
   );
