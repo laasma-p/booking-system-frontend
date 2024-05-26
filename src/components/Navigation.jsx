ec3c766585e1bc6500f9b8831fd729a64722d2af
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-26 15:50:19 +0200

    import logo and adjust Navigation component styles to fit it

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,33 @@
+import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <header className="bg-blue-700 dark:bg-blue-600 transition-all">
-      <nav className="p-4 flex justify-between items-center">
-        <div>
-          <NavLink
-            to="/bookings"
-            className="text-gray-950 hover:text-gray-100 mr-4 transition-all"
-          >
-            Bookings
-          </NavLink>
-          <NavLink
-            to="/book-a-time"
-            className="text-gray-950 hover:text-gray-100 transition-all"
-          >
-            Book a time
-          </NavLink>
+    <header className="w-full bg-blue-700 dark:bg-blue-600 flex transition-all">
+      <nav className="w-full mx-auto p-2 px-2 flex items-center">
+        <div className="w-16 h-16 flex justify-center items-center ml-2 mr-4">
+          <img src={logo} />
         </div>
-        <div>
-          <button className="text-gray-950 hover:text-gray-100 transition-all">
-            Log out
-          </button>
+        <div className="flex w-full">
+          <div className="w-10/12 border-red-200 text-lg space-x-6">
+            <NavLink
+              to="/bookings"
+              className="text-gray-950 hover:text-gray-100 transition-all"
+            >
+              Bookings
+            </NavLink>
+            <NavLink
+              to="/book-a-time"
+              className="text-gray-950 hover:text-gray-100 transition-all"
+            >
+              Book a time
+            </NavLink>
+          </div>
+          <div className="flex w-2/12 justify-end mr-2">
+            <button className="text-gray-950 text-lg hover:text-gray-100 transition-all">
+              Log out
+            </button>
+          </div>
         </div>
       </nav>
     </header>
