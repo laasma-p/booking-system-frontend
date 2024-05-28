36b402ffdebe8253885f15880868d48c8117aee7
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-28 18:47:27 +0200

    fetch booked booking

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,7 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Booking = () => {
-  const [booking, setBooking] = useState(true);
+  const [booking, setBooking] = useState(null);
+
+  useEffect(() => {
+    const fetchBooking = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch("http://localhost:3000/current-booking", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setBooking(data);
+        } else {
+          setBooking(null);
+        }
+      } catch (error) {
+        console.error("Error fetching booking", error);
+      }
+    };
+
+    fetchBooking();
+  }, []);
 
   return (
     <>
@@ -13,9 +37,9 @@ const Booking = () => {
           key="1"
           className="border max-w-lg rounded-md mx-4 p-4 transition-all"
         >
-          <p className="font-semibold">Booking Name</p>
-          <p>Language: English</p>
-          <p>Time: 10:00</p>
+          <p className="font-semibold">Booking Name: {booking.booking_name}</p>
+          <p>Language: {booking.booking_language}</p>
+          <p>Time: {booking.time}</p>
           <button
             type="button"
             className="bg-red-500 hover:bg-red-600 text-gray-100 mt-2 py-2 px-4 rounded-md transition-all"
