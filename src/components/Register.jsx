398dc2c3ee83d2f885602b8a48f88c1f7fa0c754
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-29 10:35:09 +0200

    have all inputs the same width

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -182,12 +182,7 @@ const Register = () => {
             { id: "post_code", label: "Post Code" },
           ].map((field) => {
             return (
-              <div
-                key={field.id}
-                className={`flex flex-col space-y-2 ${
-                  field.id === "birthday" ? "md:col-span-2" : ""
-                }`}
-              >
+              <div key={field.id} className="flex flex-col space-y-2">
                 <label
                   htmlFor={field.id}
                   className="text-gray-950 dark:text-gray-100"
@@ -200,7 +195,7 @@ const Register = () => {
                   value={registerData[field.id]}
                   onChange={registerDataChangeHandler}
                   onBlur={inputBlurHandler}
-                  className={`text-gray-950 dark:text-gray-100 dark:bg-gray-700 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 dark:focus:ring-blue-400 ${
+                  className={`w-full text-gray-950 dark:text-gray-100 dark:bg-gray-700 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 dark:focus:ring-blue-400 ${
                     errors[field.id] ? "border-red-400 dark:border-red-500" : ""
                   }`}
                 />
