51fda9c545ddd38154e3fa8d80ab3319c9622e9f
Lasma Poksane <laasmapoksane@gmail.com>
2024-12-17 23:47:28 +0100

    add initial hamburger menu to the navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import logo from "../assets/logo.png";
 import { NavLink, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Navigation = ({ setLoggedIn }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const logoutHandler = () => {
@@ -19,6 +21,59 @@ const Navigation = ({ setLoggedIn }) => {
         <div className="w-16 h-16 flex justify-center items-center ml-2 mr-4">
           <img src={logo} alt="Logo" />
         </div>
+
+        <button onClick={() => setIsMenuOpen((prev) => !prev)}>
+          <svg
+            className="w-8 h-8"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+
+        {isMenuOpen && (
+          <div>
+            <div className="w-10/12 border-red-200 text-lg space-x-6">
+              <NavLink
+                to="/booking"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-gray-100"
+                    : "text-gray-950 hover:text-gray-100 transition-all"
+                }
+              >
+                Booking
+              </NavLink>
+              <NavLink
+                to="/book-a-time"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-gray-100"
+                    : "text-gray-950 hover:text-gray-100 transition-all"
+                }
+              >
+                Book a time
+              </NavLink>
+            </div>
+            <div className="flex w-2/12 justify-end mr-2">
+              <button
+                className="text-gray-950 text-lg hover:text-gray-100 transition-all"
+                onClick={logoutHandler}
+              >
+                Log out
+              </button>
+            </div>
+          </div>
+        )}
+
         <div className="flex w-full">
           <div className="w-10/12 border-red-200 text-lg space-x-6">
             <NavLink
