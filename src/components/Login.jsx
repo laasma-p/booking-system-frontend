9e97ee6d58581457cfd97ff65daea61572acece4
Lasma Poksane <laasmapoksane@gmail.com>
2024-12-18 04:13:19 +0100

    adjust Login component styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -50,17 +50,14 @@ const Login = ({ setLoggedIn }) => {
   };
 
   return (
-    <div className="min-h-dvh flex items-center flex-col justify-center bg-gray-100 dark:bg-gray-900">
-      <h1 className="text-3xl font-semibold text-gray-950 dark:text-gray-100 text-center mb-6">
-        Log In
-      </h1>
-      <form
-        className="w-5/6 max-w-lg bg-white dark:bg-gray-800 p-6 md:p-8 rounded-md shadow-md"
-        onSubmit={loginHandler}
-      >
-        <div className="flex flex-col space-y-4">
+    <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-12 bg-gray-50">
+      <div className="w-full max-w-md bg-gray-100 p-4 sm:p-6 md:p-8 rounded-lg shadow-lg border border-gray-300">
+        <h1 className="text-3xl font-semibold text-center text-gray-950 mb-6">
+          Log In
+        </h1>
+        <form className="space-y-6" onSubmit={loginHandler}>
           <div className="flex flex-col space-y-2">
-            <label htmlFor="email" className="text-gray-950 dark:text-gray-100">
+            <label htmlFor="email" className="text-gray-950">
               E-mail
             </label>
             <input
@@ -68,14 +65,11 @@ const Login = ({ setLoggedIn }) => {
               id="email"
               value={loginData.email}
               onChange={loginDataChangeHandler}
-              className="text-gray-950 dark:text-gray-100 dark:bg-gray-700 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+              className="w-full text-gray-950 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
             />
           </div>
           <div className="flex flex-col space-y-2">
-            <label
-              htmlFor="password"
-              className="text-gray-950 dark:text-gray-100"
-            >
+            <label htmlFor="password" className="text-gray-950">
               Password
             </label>
             <input
@@ -83,27 +77,27 @@ const Login = ({ setLoggedIn }) => {
               id="password"
               value={loginData.password}
               onChange={loginDataChangeHandler}
-              className="text-gray-950 dark:text-gray-100 dark:bg-gray-700 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+              className="w-full text-gray-950 px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
             />
           </div>
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-700 dark:bg-blue-600 hover:bg-blue-800 dark:hover:bg-blue-700 text-gray-100 mt-6 py-3 rounded-md md:text-lg lg:text-xl transition-all"
-        >
-          Log In
-        </button>
-        <p className="mt-4 text-center text-gray-950 dark:text-gray-100">
-          Do not have an account?
-          <br />
-          <Link
-            to="/register"
-            className="text-blue-700 dark:text-blue-600 hover:text-blue-800 dark:hover:text-blue-700 transition-all"
+          <button
+            type="submit"
+            className="w-full bg-blue-700 hover:bg-blue-800 text-gray-100 mt-6 py-3 rounded-md md:text-lg lg:text-xl transition-all"
           >
-            Register here
-          </Link>
-        </p>
-      </form>
+            Log In
+          </button>
+          <p className="mt-4 text-center text-gray-950">
+            Do not have an account?
+            <br />
+            <Link
+              to="/register"
+              className="text-blue-700 hover:text-blue-800 transition-all"
+            >
+              Register here
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
